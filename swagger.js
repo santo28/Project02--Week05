3741c6bcf7342b06027286aacd0c859e5f61d92a
santo28 <santos17004@byui.edu>
2023-01-28 07:19:32 -0700

    new changes to render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'Temple API'
   },
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: 'https://cse341-lesson04.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
