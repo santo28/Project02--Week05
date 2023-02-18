e75223fefd60e8328c8642c3ab8dc21639ef1713
santo28 <santos17004@byui.edu>
2023-02-17 19:17:37 -0700

    w5 render back change

--- swagger.js
+++ swagger.js
@@ -15,7 +15,3 @@ const endpointsFiles = ['./routes/index.js'];
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
 
-// Run server after it gets generated
-// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
-//   await import('./index.js');
-// });
